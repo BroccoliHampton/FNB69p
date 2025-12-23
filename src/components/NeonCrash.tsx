cc33bc8800fc5aa5b2c8153467a046dd7fa8cd3a
BroccoliHampton <137211602+BroccoliHampton@users.noreply.github.com>
2025-12-23 15:39:52 -0800

    Update NeonCrash.tsx

--- src/components/NeonCrash.tsx
+++ src/components/NeonCrash.tsx
@@ -1,63 +1,55 @@
+
 import React from 'react';
 
-const NeonCrash: React.FC = () => {
+const FnbText3D: React.FC = () => {
+  const text = "FNB69P";
+  const depth = 10; // Number of layers for 3D effect
+
   return (
     <div 
-      className="relative neon-crash" 
+      className="relative flex items-center justify-center neon-crash" 
       style={{ 
         perspective: '1000px', 
-        width: '200px', 
-        height: '300px',
+        width: '400px', 
+        height: '200px',
         transformStyle: 'preserve-3d'
       }}
     >
-      {/* Crude Head */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-24 h-24 bg-mine-orange border-4 border-white" 
-           style={{ transform: 'translateZ(40px) rotateX(-10deg)' }}>
-        {/* Mouth/Muzzle */}
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-20 h-10 bg-[#FFC0CB] border-2 border-black" style={{ transform: 'translateZ(10px)' }}>
-          <div className="w-full h-4 bg-white border-b-2 border-black mt-1"></div>
-        </div>
-        {/* Eyes */}
-        <div className="absolute top-4 left-2 w-8 h-10 bg-white border-2 border-black rounded-full flex items-center justify-center">
-          <div className="w-2 h-2 bg-black rounded-full"></div>
-        </div>
-        <div className="absolute top-4 right-2 w-8 h-10 bg-white border-2 border-black rounded-full flex items-center justify-center">
-          <div className="w-2 h-2 bg-black rounded-full"></div>
-        </div>
-        {/* Ears */}
-        <div className="absolute -top-10 -left-6 w-10 h-16 bg-mine-orange border-4 border-black" style={{ clipPath: 'polygon(100% 100%, 0 100%, 50% 0)' }}></div>
-        <div className="absolute -top-10 -right-6 w-10 h-16 bg-mine-orange border-4 border-black" style={{ clipPath: 'polygon(100% 100%, 0 100%, 50% 0)' }}></div>
-        {/* Hair spikes */}
-        <div className="absolute -top-8 left-1/2 -translate-x-1/2 w-4 h-12 bg-red-600 border-2 border-black rotate-12"></div>
-        <div className="absolute -top-10 left-1/2 -translate-x-1/2 w-4 h-12 bg-red-600 border-2 border-black -rotate-12"></div>
+      {/* Container for the 3D text */}
+      <div 
+        className="relative"
+        style={{ transformStyle: 'preserve-3d' }}
+      >
+        {/* Layered text to create 3D depth */}
+        {Array.from({ length: depth }).map((_, i) => (
+          <span
+            key={i}
+            className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl font-black italic select-none
+              ${i === depth - 1 ? 'text-white' : 'text-mine-orange opacity-80'}
+            `}
+            style={{
+              transform: `translate3d(-50%, -50%, ${i * 4}px)`,
+              WebkitTextStroke: i === depth - 1 ? '2px #000' : '1px #FFF',
+              textShadow: i === depth - 1 ? '0 0 20px #FF4500' : 'none',
+              whiteSpace: 'nowrap'
+            }}
+          >
+            {text}
+          </span>
+        ))}
+        
+        {/* Additional Neon Glow Elements */}
+        <div 
+          className="absolute -inset-10 border-8 border-mine-blue opacity-30 animate-pulse"
+          style={{ transform: 'translateZ(-20px) rotateX(20deg)' }}
+        ></div>
+        <div 
+          className="absolute -inset-4 border-4 border-mine-green opacity-50"
+          style={{ transform: 'translateZ(50px) rotateY(-10deg)' }}
+        ></div>
       </div>
-
-      {/* Crude Body */}
-      <div className="absolute top-24 left-1/2 -translate-x-1/2 w-16 h-32 bg-mine-orange border-4 border-white" style={{ transform: 'translateZ(10px) rotateX(10deg)' }}>
-        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-12 h-20 bg-mine-orange brightness-125"></div>
-      </div>
-
-      {/* Arms */}
-      <div className="absolute top-28 -left-8 w-10 h-24 bg-mine-orange border-2 border-white rotate-[30deg]" style={{ transform: 'translateZ(-10px)' }}></div>
-      <div className="absolute top-28 -right-8 w-10 h-24 bg-mine-orange border-2 border-white rotate-[-30deg]" style={{ transform: 'translateZ(-10px)' }}></div>
-
-      {/* Gloves */}
-      <div className="absolute top-44 -left-12 w-12 h-12 bg-red-800 border-2 border-black" style={{ transform: 'translateZ(20px)' }}></div>
-      <div className="absolute top-44 -right-12 w-12 h-12 bg-red-800 border-2 border-black" style={{ transform: 'translateZ(20px)' }}></div>
-
-      {/* Pants (Blue) */}
-      <div className="absolute top-52 left-1/2 -translate-x-1/2 w-20 h-16 bg-mine-blue border-4 border-white" style={{ transform: 'translateZ(30px)' }}></div>
-
-      {/* Legs/Feet */}
-      <div className="absolute top-64 left-4 w-12 h-20 bg-mine-orange border-2 border-black"></div>
-      <div className="absolute top-64 right-4 w-12 h-20 bg-mine-orange border-2 border-black"></div>
-      
-      {/* Shoes */}
-      <div className="absolute top-80 left-0 w-16 h-8 bg-red-600 border-4 border-black rounded-full" style={{ transform: 'translateZ(40px)' }}></div>
-      <div className="absolute top-80 right-0 w-16 h-8 bg-red-600 border-4 border-black rounded-full" style={{ transform: 'translateZ(40px)' }}></div>
     </div>
   );
 };
 
-export default NeonCrash;
+export default FnbText3D;
